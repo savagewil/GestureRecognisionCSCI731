d747b57b766efa4b756e5c2dc41cd44342d94460
savagewil <willsvg@gmail.com>
2021-03-01 16:00:00 -0500

    Setting up skin classifier

--- SettingUpRequirements.py
+++ SettingUpRequirements.py
@@ -19,7 +19,30 @@ while True:
     if not ret:
         print("failed to grab frame")
         break
-    cv2.imshow("test", frame)
+
+    frames = []
+
+    frames.append(cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)[:, :, 0])
+    frames.append(cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)[:, :, 1])
+    frames.append(cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)[:, :, 2])
+
+    frames.append(cv2.cvtColor(frame, cv2.COLOR_BGR2LAB)[:, :, 0])
+    frames.append(cv2.cvtColor(frame, cv2.COLOR_BGR2LAB)[:, :, 1])
+    frames.append(cv2.cvtColor(frame, cv2.COLOR_BGR2LAB)[:, :, 2])
+
+    frames.append(cv2.cvtColor(frame, cv2.COLOR_BGR2YUV)[:, :, 0])
+    frames.append(cv2.cvtColor(frame, cv2.COLOR_BGR2YUV)[:, :, 1])
+    frames.append(cv2.cvtColor(frame, cv2.COLOR_BGR2YUV)[:, :, 2])
+
+    # frames[1] = numpy.mean(cv2.cvtColor(frames[1], cv2.COLOR_BGR2HSV), axis=1)
+    # frames[1] = cv2.cvtColor(frames[1], cv2.COLOR_BGR2HSV)[..., 2]
+    # frames[2] = cv2.cvtColor(frames[2], cv2.COLOR_BGR2LAB)[..., 0]
+    # frames[3] = cv2.cvtColor(frames[3], cv2.COLOR_BGR2YUV)[..., 0]
+
+    panel = cv2.vconcat([cv2.hconcat([frames[0], frames[1], frames[2]]),
+                         cv2.hconcat([frames[3], frames[4], frames[5]]),
+                         cv2.hconcat([frames[6], frames[7], frames[8]])])
+    cv2.imshow("test", panel)
 
     k = cv2.waitKey(1)
     if k%256 == 27:
